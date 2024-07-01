ed27fab2ff654b71fefdf63c0e341a3e9a0142eb
Ibrahimhowlader2 <sibrahim.ih2@gmail.com>
2024-07-01 20:13:45 +0600

    home page text added

--- heartland_hospital/urls.py
+++ heartland_hospital/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('contact_us/', include('contact_us.urls')),
     path('services/', include('service.urls')),
     path('patient/', include('patient.urls')),
